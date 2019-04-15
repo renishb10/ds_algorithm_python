339ca9764a0f8f0141c25164a6fddb8b6f3cdca9
Renish B <renish.bhaskaran@globalenglish.com>
2019-04-15 22:02:23 +0530

    added anagram_check2

--- DS_Python/problems/02_anagram_check.py
+++ DS_Python/problems/02_anagram_check.py
@@ -1,20 +1,55 @@
+import time
+
+#Using Sorting and Comparing
 def check_anagram_1(s1, s2):
     s1 = s1.replace(" ", "")
     s2 = s2.replace(" ", "")
 
     if len(s1) != len(s2):
         print("Given strings are not in Equal length!")
+        return False
     else:
         s1 = ''.join(sorted(s1.upper()))
         s2 = ''.join(sorted(s2.upper()))
 
-        isAnagram = True
         for c in range(0, len(s1)):
             if s1[c] != s2[c]:
-                isAnagram = False
-                break
+                return False
 
-        return isAnagram
+        return True
+
+#Using Dictionary and Comparing
+def check_anagram_2(s1, s2):
+    s1 = s1.replace(" ", "").lower()
+    s2 = s2.replace(" ", "").lower()
+
+    if len(s1) != len(s2):
+        print("Given strings are not in Equal length!")
+        return False
+    else:
+        hash = {}
+        for letter in s1:
+            if letter in hash:
+                hash[letter] += 1
+            else:
+                hash[letter] = 1
+
+        for letter in s2:
+            if letter in hash:
+                hash[letter] -= 1
+            else:
+                hash[letter] = 1
+
+        for k in hash:
+            if hash[k] != 0:
+                return False
+
+        return True
 
 print(check_anagram_1('dog','God'))
 print(check_anagram_1('clint Eastwood','old west action'))
+
+print('--------------------------------------------------')
+
+print(check_anagram_2('dog','God'))
+print(check_anagram_2('clint Eastwood','old west action'))
