8ab9077f05336edfd2cd4c9e8c1dfd92c18c84c6
Renish B <renish.bhaskaran@globalenglish.com>
2019-05-31 23:54:36 +0530

    added ds stack

---
+++ DS_JS/ds/stack_array.js
@@ -0,0 +1,55 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(data) {
+    if (data) {
+      this.items.push(data);
+    }
+  }
+
+  pop() {
+    if (this.items.length <= 0) {
+      throw new Error("Stack underflow");
+    }
+    const poppedItem = this.items.pop();
+    console.log('pop: ', poppedItem);
+    return poppedItem;
+  }
+
+  peek() {
+    if (this.items.length <= 0) {
+      throw new Error("Stack underflow");
+    }
+    console.log('peek: ', this.items[this.items.length - 1]);
+  }
+
+  size() {
+    console.log('size: ',this.items.length);
+  }
+
+  display() {
+    if (this.items.length <= 0) {
+      throw new Error("Stack underflow");
+    }
+    this.items.forEach(item => {
+      console.log(item);
+    });
+  }
+}
+
+const newStack = new Stack();
+newStack.push(1);
+newStack.push(2);
+newStack.push(3);
+newStack.push(4);
+newStack.push(5);
+
+newStack.display();
+
+newStack.pop();
+
+newStack.size();
+
+newStack.display();
